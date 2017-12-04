948895a51926375daad2aaf61727dacd057ebe14
Prashant Budania <prashantbudania250@gmail.com>
2017-12-03 22:46:16 -0500

    Dynet installation flies

---
+++ install_dynet.bash
@@ -0,0 +1,12 @@
+# 17 Nov 2017
+# By: Aldrian Obaja
+# The script to install Dynet
+
+# Need to change to CUDA 8 because DyNet doesn't work in CUDA 9
+# https://github.com/clab/dynet/issues/988
+sudo rm /usr/local/cuda
+sudo ln -s /usr/local/cuda-8.0 /usr/local/cuda
+
+# Make sure we compile dynet with GPU support
+export BACKEND=cuda
+pip install --user -r requirements-dynet.txt
